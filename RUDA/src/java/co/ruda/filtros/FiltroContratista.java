417eb140cd1804acb1e0b02228d2697499f1d23d
TUL-00041FCLEM <TUL-00041FCLEM@TULDFPCLE1F6146>
2016-02-23 07:52:44 -0500

    modificcion de cristian

---
+++ RUDA/src/java/co/ruda/filtros/FiltroContratista.java
@@ -0,0 +1,57 @@
+
+package co.ruda.filtros;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Instructor;
+
+public class FiltroContratista implements Filter{
+
+    private FilterConfig configuration;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.configuration=filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try{
+        String tipo=((HttpServletRequest) request).getSession().getAttribute("tipo").toString();
+        System.out.println("Tipo "+tipo);
+        if(tipo!=null)
+        {
+            if(!tipo.equals("instructor"))
+            {
+               // Instuctor instructor = (Instuctor) ((HttpServletRequest)request).getSession().getAttribute("usuario");
+                ((HttpServletResponse)response).sendRedirect("faces/SesionInvalida.xhtml");
+            }else{
+                Instructor instructor =(Instructor)((HttpServletRequest)request).getSession().getAttribute("usuario");
+            
+                if(instructor!=null){
+                    chain.doFilter(request, response);
+                }else{
+                    ((HttpServletResponse)response).sendRedirect("faces/SesionInvalida.xhtml");
+                }
+            
+            }
+        }
+        }catch(NullPointerException e){
+            ((HttpServletResponse)response).sendRedirect("faces/SesionInvalida.xhtml");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        this.configuration = null;
+        
+    }
+    
+}
